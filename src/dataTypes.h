19e5ba479c09e88d33d136f85ed512391225721a
ExPikaPaka <112851715+ExPikaPaka@users.noreply.github.com>
2024-02-05 18:50:46 +0200

    Added data types

---
+++ src/dataTypes.h
@@ -0,0 +1,69 @@
+#pragma once
+#include <cstdint>
+#include <glm/glm.hpp>
+
+/*********************************************************/
+/* Integer values                                        */
+/*********************************************************/
+typedef int8_t i8; // 8-bit signed integer
+typedef int16_t i16; // 16-bit signed integer
+typedef int32_t i32; // 32-bit signed integer
+typedef int64_t i64; // 64-bit signed integer
+typedef uint8_t ui8; // 8-bit unsigned integer
+typedef uint16_t ui16; // 16-bit unsigned integer
+typedef uint32_t ui32; // 32-bit unsigned integer
+typedef uint64_t ui64; // 64-bit unsigned integer
+typedef unsigned int uint; // unsigned integer
+
+/*********************************************************/
+/* Floating point values                                 */
+/*********************************************************/
+typedef float f32; // 32-bit floating point value
+typedef double f64; // 64-bit floating point value
+
+/*********************************************************/
+/* Ent types                                             */
+/*********************************************************/
+typedef int8_t i8; // 8-bit signed integer
+typedef int16_t i16; // 16-bit signed integer
+typedef int32_t i32; // 32-bit signed integer
+typedef int64_t i64; // 64-bit signed integer
+
+typedef glm::i8vec2 i8v2; // 8-bit signed integer 2-value vector
+typedef glm::i16vec2 i16v2; // 16-bit signed integer 2-value vector
+typedef glm::i32vec2 i32v2; // 32-bit signed integer 2-value vector
+typedef glm::i64vec2 i64v2; // 64-bit signed integer 2-value vector
+typedef glm::u8vec2 ui8v2; // 8-bit unsigned integer 2-value vector
+typedef glm::u16vec2 ui16v2; // 16-bit unsigned integer 2-value vector
+typedef glm::u32vec2 ui32v2; // 32-bit unsigned integer 2-value vector
+typedef glm::u64vec2 ui64v2; // 64-bit unsigned integer 2-value vector
+typedef glm::f32vec2 f32v2; // 32-bit floating point 2-value vector
+typedef glm::f64vec2 f64v2; // 64-bit floating point 2-value vector
+typedef glm::f32mat2 f32m2; /// 32-bit floating point 2x2 matrix
+typedef glm::f64mat2 f64m2; // 64-bit floating point 2x2 matrix
+
+typedef glm::i8vec3 i8v3; // 8-bit signed integer 3-value vector
+typedef glm::i16vec3 i16v3; // 16-bit signed integer 3-value vector
+typedef glm::i32vec3 i32v3; // 32-bit signed integer 3-value vector
+typedef glm::i64vec3 i64v3; // 64-bit signed integer 3-value vector
+typedef glm::u8vec3 ui8v3; // 8-bit unsigned integer 3-value vector
+typedef glm::u16vec3 ui16v3; // 16-bit unsigned integer 3-value vector
+typedef glm::u32vec3 ui32v3; // 32-bit unsigned integer 3-value vector
+typedef glm::u64vec3 ui64v3; // 64-bit unsigned integer 3-value vector
+typedef glm::f32vec3 f32v3; // 32-bit floating point 3-value vector
+typedef glm::f64vec3 f64v3; // 64-bit floating point 3-value vector
+typedef glm::f32mat3 f32m3; /// 32-bit floating point 3x3 matrix
+typedef glm::f64mat3 f64m3; // 64-bit floating point 3x3 matrix
+
+typedef glm::i8vec4 i8v4; // 8-bit signed integer 4-value vector
+typedef glm::i16vec4 i16v4; // 16-bit signed integer 4-value vector
+typedef glm::i32vec4 i32v4; // 32-bit signed integer 4-value vector
+typedef glm::i64vec4 i64v4; // 64-bit signed integer 4-value vector
+typedef glm::u8vec4 ui8v4; // 8-bit unsigned integer 4-value vector
+typedef glm::u16vec4 ui16v4; // 16-bit unsigned integer 4-value vector
+typedef glm::u32vec4 ui32v4; // 32-bit unsigned integer 4-value vector
+typedef glm::u64vec4 ui64v4; // 64-bit unsigned integer 4-value vector
+typedef glm::f32vec4 f32v4; // 32-bit floating point 4-value vector
+typedef glm::f64vec4 f64v4; // 64-bit floating point 4-value vector
+typedef glm::f32mat4 f32m4; /// 32-bit floating point 4x4 matrix
+typedef glm::f64mat4 f64m4; // 64-bit floating point 4x4 matrix
