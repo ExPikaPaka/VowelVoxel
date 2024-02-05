65cc3cf3354b036f4a5704b96c216c8735841fe9
ExPikaPaka <112851715+ExPikaPaka@users.noreply.github.com>
2024-02-05 18:52:30 +0200

    Added timer class

---
+++ src/Utility/Timer.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+namespace ent {
+	// Utility namespace
+	namespace util {
+		// Timer class.
+		class Timer {
+			unsigned int endTime;
+
+			// Returns current time in milliseconds
+			unsigned int getTime();
+		public:
+			Timer();
+
+			// Sets timer active till endTime in milliseconds
+			void setTimer(int endTimeMilliseconds);
+
+			// Returns 'true' if currentTime < endTime. Otherwise returns 'false'
+			bool active();
+
+			// Sets renderEnd to 0, to make active() return 'false'
+			void clear();
+		};
+	}
+}
